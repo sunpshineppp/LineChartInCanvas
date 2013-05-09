765ff5c99574e53d89a6ede6e805d6fd4c64a837
sunpshineppp <sunpshineppp@gmail.com>
2013-05-09 20:41:15 +0800

    step 2 : draw a line to connect those points

--- src/com/sunpshine/linechartincanvas/CreateLineChart.java
+++ src/com/sunpshine/linechartincanvas/CreateLineChart.java
@@ -47,9 +47,9 @@ public class CreateLineChart extends View{
 		Rect y_axis = new Rect();
 		y_axis.set(x0,yEnd,x0+5,y0);
 		Paint y_axis_color = new Paint();
-		y_axis_color.setColor(Color.BLUE);
+		y_axis_color.setColor(Color.RED);
 		canvas.drawRect(y_axis,y_axis_color);
-		
+			
 		// draw data
 		Paint point_color = new Paint();
 		point_color.setColor(Color.GREEN);
@@ -63,6 +63,13 @@ public class CreateLineChart extends View{
 		canvas.drawCircle(x0+60, y0-80, 3, point_color);
 		canvas.drawCircle(x0+70, y0-50, 3, point_color);
 		canvas.drawCircle(x0+80, y0-450, 3, point_color);
+		
+		// draw line
+		Paint line_color = new Paint();
+		line_color.setColor(Color.BLUE);
+		
+		canvas.drawLine(x0, y0, x0+10, y0-260, line_color);
+		canvas.drawLine(x0+10, y0-260, x0+20, y0-290, line_color);
 		
 		
 		// start draw
